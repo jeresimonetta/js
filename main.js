ae299a6cb6ea0bf30bf6b30a1648039acfdc8ef0
Jeremias Simonetta <jeresimon86@gmail.com>
2024-01-11 01:36:31 -0300

    Condicional y ciclo listo

--- main.js
+++ main.js
@@ -4,9 +4,37 @@ let sueldo = prompt("Por favor indicanos tu sueldo (en pesos y sin centavos).")
 if(sueldo < 100000){
     alert(nombre + " lamentablemente no contamos con opciones de créditos para vos.")
 }
-else if(sueldo >= 100000 && sueldo < 500000){
-    alert(nombre + " tenés disponible un préstamo de $" + sueldo*3 + ". Tocá aceptar para continuar.")
-}
 else{
-    alert(nombre + " tenés disponible un préstamo de $" + sueldo*5 + ". Tocá aceptar para continuar.")
+    if(sueldo >= 100000 && sueldo < 500000){
+        alert(nombre + " tenés disponible un préstamo de $" + sueldo*3 + ". Tocá aceptar para continuar.")
+        let prestamo = prompt("¿Cuál es monto de tu préstamo?")
+        while(prestamo > sueldo*3){
+            alert(nombre + " el monto indicado excede tu disponible.")
+            prestamo = prompt("¿Cuál es monto de tu préstamo?")
+        }
+    }
+    else{
+        alert(nombre + " tenés disponible un préstamo de $" + sueldo*5 + ". Tocá aceptar para continuar.")
+        let prestamo = prompt("¿Cuál es monto de tu préstamo?")
+        while(prestamo > sueldo*5){
+            alert(nombre + " el monto indicado excede tu disponible.")
+            prestamo = prompt("¿Cuál es monto de tu préstamo?")
+        }
+    }
+
+    let anios = prompt("Indicanos por favor en cuantos años querés devolver tu préstamo.")
+
+    while(anios > 4){
+        alert("El plazo máximo de devolución es de cuatro años.")
+        anios = prompt("Indicanos por favor en cuantos años querés devolver tu préstamo.")
+    }
+
+    function valorCuota(prestamo, anios){
+        return prestamo/(anios*12)
+    }
+
+    alert("Devolverás tu préstamo en " + anios*12 + " cuotas de $" + valorCuota(prestamo, anios))
+
 }
+
+
